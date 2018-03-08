1204481f1e0ea8926e17bcc3bcf1b3fa1cc9142a
autodocs <autodocs>
2018-03-08 21:08:21 +0000

    build based on 74e2ead

---
+++ latest/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "chap01.html#",
+    "page": "The way of the program",
+    "title": "The way of the program",
+    "category": "page",
+    "text": "The goal of this book is to teach you to think like a computer scientist. This way of thinking combines some of the best features of mathematics, engineering, and natural science.  Like mathematicians, computer scientists use formal languages to denote ideas (specifically computations).  Like engineers, they design things, assembling components into systems and evaluating tradeoffs among alternatives. Like scientists, they observe the behavior of complex systems, form hypotheses, and test predictions.The single most important skill for a computer scientist is problem solving. Problem solving means the ability to formulate problems, think creatively about solutions, and express a solution clearly and accurately.  As it turns out, the process of learning to program is an excellent opportunity to practice problem-solving skills.  That\'s why this chapter is called, ``The way of the program\'\'.On one level, you will be learning to program, a useful skill by itself.  On another level, you will use programming as a means to an end.  As we go along, that end will become clearer."
+},
+
+{
+    "location": "chap01.html#What-is-a-program?-1",
+    "page": "The way of the program",
+    "title": "What is a program?",
+    "category": "section",
+    "text": "A program is a sequence of instructions that specifies how to perform a computation. The computation might be something mathematical, such as solving a system of equations or finding the roots of a polynomial, but it can also be a symbolic computation, such as searching and replacing text in a document or something graphical, like processing an image or playing a video.The details look different in different languages, but a few basic instructions appear in just about every language:"
+},
+
+]}
