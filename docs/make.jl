01c093513439c14e17ee058997489f7c6ad6e9b5
Ben Lauwens <ben.lauwens@gmail.com>
2018-03-08 21:21:09 +0100

    Initial commit

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter
+
+makedocs(
+  format = :html,
+  sitename = "Think Julia",
+  authors = "Ben Lauwens",
+  pages = [
+    "Home" => "index.md",
+    "The way of the program" => "chap01.md"
+  ]
+)
+
+deploydocs(
+  repo = "github.com/BenLauwens/ThinkJulia.jl",
+  target = "build",
+  deps   = nothing,
+  make   = nothing
+)
