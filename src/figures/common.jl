c11969e4f46b08412d48d6161ac78b86761b0e22
Ben Lauwens <ben.lauwens@gmail.com>
2018-03-22 12:42:47 +0100

    Use monofont Hack

--- src/figures/common.jl
+++ src/figures/common.jl
@@ -1,7 +1,7 @@
 const preamble = """
 \\usepackage{cancel}
 \\usepackage{fontspec}
-\\setmonofont[Scale=MatchLowercase]{Inconsolata}
+\\setmonofont[Scale=MatchLowercase]{Hack}
 """
 
 const options = "thick, scale=1.3, transform shape"
