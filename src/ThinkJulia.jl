db940f09931ea7e39bfed7a15ab0a4d0ab77fa5a
Bart Janssens <bjanssens@mbair-bart.local>
2019-01-05 22:44:54 +0100

    Fix some typos

--- src/ThinkJulia.jl
+++ src/ThinkJulia.jl
@@ -27,7 +27,7 @@ module ThinkJulia
 
   export printlyrics, repeatlyrics, printtwice, cattwice
   export rightjustify, printgrid
-  export Turtle, forward, turn, penup, pendown, polyline, polygon, square, arc, cercle, @svg
+  export Turtle, forward, turn, penup, pendown, @svg
   #export countdown, printn, recurse
   #export area, absvalue, distance, circlearea, isdivisible, fact, fib
   export mysqrt, testsquareroot
