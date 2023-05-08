1b0b4c684128afd497702aecf3ff716f40a851be
cormullion <cormullion@mac.com>
2023-05-08 14:30:28 +0100

    try to update to Julia 1.9

--- src/ThinkJulia.jl
+++ src/ThinkJulia.jl
@@ -1,7 +1,5 @@
 module ThinkJulia
 
-  __precompile__(false)
-
   using Printf
   using Random
   using Base64
@@ -9,16 +7,10 @@ module ThinkJulia
   using Luxor
   using TikzPictures
   using Plots
-
-  const depsjl_path = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-  if !isfile(depsjl_path)
-      error("ThinkJulia not installed properly, run Pkg.build(\"ThinkJulia\"), restart Julia and try again")
-  end
-  include(depsjl_path)
+  using Gdbm_jll
 
   function __init__()
     # Always check your dependencies that live in `deps.jl`
-    check_deps()
     global setcolor! = Core.eval(Base, :(x -> (y = have_color; global have_color = x; y)))
     CAN_INLINE[] = Base.JLOptions().can_inline == 0 ? false : true
   end
@@ -27,7 +19,7 @@ module ThinkJulia
 
   export printlyrics, repeatlyrics, printtwice, cattwice
   export rightjustify, printgrid
-  export Turtle, forward, turn, penup, pendown, @svg
+  export Turtle, forward, turn, penup, pendown, @svg, @drawsvg, @png, background
   #export countdown, printn, recurse
   #export area, absvalue, distance, circlearea, isdivisible, fact, fib
   export mysqrt, testsquareroot
--- src/code/chap14.jl
+++ src/code/chap14.jl
@@ -20,71 +20,53 @@ const OPENFLAGS = Dict("r"=>0, "w"=>1, "c"=>2, "n"=>3)
 const STOREFLAGS = Dict("r"=>1, "i"=>0)
 
 function gdbm_open(name::String, flag::String="r")
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_open = Libdl.dlsym_e(GDBM, :gdbm_open)
-  handle = ccall(gdbm_open, Ptr{Cvoid}, (Cstring, Int32, Int32, Int32, Ptr{Cvoid}), name, 0, OPENFLAGS[flag], 420, C_NULL)
+  handle = ccall((:gdbm_open, libgdbm), Ptr{Cvoid}, (Cstring, Int32, Int32, Int32, Ptr{Cvoid}), name, 0, OPENFLAGS[flag], 420, C_NULL)
   handle == C_NULL && error("File could not be opened!")
   handle
 end
 
 function gdbm_close(handle::Ptr{Cvoid})
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_close = Libdl.dlsym_e(GDBM, :gdbm_close)
-  ccall(gdbm_close, Cvoid, (Ptr{Cvoid},), handle)
+  ccall((:gdbm_close, libgdbm), Cvoid, (Ptr{Cvoid},), handle)
 end
 
 function gdbm_store(handle::Ptr{Cvoid}, key::Datum, value::Datum, flag::String="r")
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_store = Libdl.dlsym_e(GDBM, :gdbm_store)
-  ret = ccall(gdbm_store, Int32, (Ptr{Cvoid}, Datum, Datum, Int32), handle, key, value, STOREFLAGS[flag])
+  ret = ccall((:gdbm_store, libgdbm), Int32, (Ptr{Cvoid}, Datum, Datum, Int32), handle, key, value, STOREFLAGS[flag])
   ret == -1 && error("Database is not writable or key/value is not a valid string.")
   ret == 1 && error("Key is already in database.")
   nothing
 end
 
 function gdbm_fetch(handle::Ptr{Cvoid}, key::Datum)
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_fetch = Libdl.dlsym_e(GDBM, :gdbm_fetch)
-  datum = ccall(gdbm_fetch, Datum, (Ptr{Cvoid}, Datum), handle, key)
+  datum = ccall((:gdbm_fetch, libgdbm), Datum, (Ptr{Cvoid}, Datum), handle, key)
   datum.dptr == C_NULL && throw(KeyError(key))
   datum
 end
 
 function gdbm_exists(handle::Ptr{Cvoid}, key::Datum)
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_exists = Libdl.dlsym_e(GDBM, :gdbm_exists)
-  ret = ccall(gdbm_exists, Int32, (Ptr{Cvoid}, Datum), handle, key)
+  ret = ccall((:gdbm_exists, libgdbm), Int32, (Ptr{Cvoid}, Datum), handle, key)
   ret == 0 && return false
   true
 end
 
 function gdbm_count(handle::Ptr{Cvoid})
   count = Ref(UInt(0))
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_count = Libdl.dlsym_e(GDBM, :gdbm_count)
-  ret = ccall(gdbm_count, Int32, (Ptr{Cvoid}, Ref{UInt}), handle, count)
+  ret = ccall((:gdbm_count, libgdbm), Int32, (Ptr{Cvoid}, Ref{UInt}), handle, count)
   ret == -1 && error("Error reading database.")
   Int(count[])
 end
 
 function gdbm_delete(handle::Ptr{Cvoid}, key::Datum)
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_delete = Libdl.dlsym_e(GDBM, :gdbm_delete)
-  ret = ccall(gdbm_delete, Int32, (Ptr{Cvoid}, Datum), handle, key)
+  ret = ccall((:gdbm_delete, libgdbm), Int32, (Ptr{Cvoid}, Datum), handle, key)
   ret ≠ 0 && error("Database is not writable or key not found.")
   nothing
 end
 
 function gdbm_firstkey(handle::Ptr{Cvoid})
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_firstkey = Libdl.dlsym_e(GDBM, :gdbm_firstkey)
-  ccall(gdbm_firstkey, Datum, (Ptr{Cvoid}, ), handle)
+  ccall((:gdbm_firstkey, libgdbm), Datum, (Ptr{Cvoid}, ), handle)
 end
 
 function gdbm_nextkey(handle::Ptr{Cvoid}, prev::Datum)
-  GDBM = Libdl.dlopen_e(libgdbm)
-  gdbm_nextkey = Libdl.dlsym_e(GDBM, :gdbm_nextkey)
-  ccall(gdbm_nextkey, Datum, (Ptr{Cvoid}, Datum), handle, prev)
+  ccall((:gdbm_nextkey, libgdbm), Datum, (Ptr{Cvoid}, Datum), handle, prev)
 end
 
 function Base.getindex(dbm::DBM, key::Union{String,Array{UInt8,1}})
